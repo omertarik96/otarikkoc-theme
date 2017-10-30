67aef6ec7a909daba7bd211658fdb42ecd81263a
Omer Tarik <omertarik96@gmail.com>
2017-10-30 04:33:28 -0500

    page header promo updated

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,14 +1,15 @@
 <div class="wrapper">
   <div class="page-header-image bg-main-color" data-parallax="true">
     <div class="container container-promo">
-      <h3>Get Free 5 Days Course </h3>
-      <p>Some Details and Summary About the Exam. We will provide the best resources for you</p>
-      <div class="form-group">
-        <input class="cta-input form-control form-control-transparent" type="text" value="" placeholder="name@email.com"
-               class="form-control">
-        <button class="btn btn-success cta-button"> Submit</button>
-      </div>
-      </span>
+        <h4> Sınavlara hazırlık ve kariyer konusunda Türkiye'nin 1 numarası! YKS, KPSS, DGS ve daha birçok sınav için rehberler, konu anlatımları ve çalışma programları!</h4>
+      {{--<h3>Get Free 5 Days Course </h3>--}}
+      {{--<p>Some Details and Summary About the Exam. We will provide the best resources for you</p>--}}
+      {{--<div class="form-group">--}}
+        {{--<input class="cta-input form-control form-control-transparent" type="text" value="" placeholder="name@email.com"--}}
+               {{--class="form-control">--}}
+        {{--<button class="btn btn-success cta-button"> Submit</button>--}}
+      {{--</div>--}}
+      {{--</span>--}}
     </div>
   </div>
 </div>
